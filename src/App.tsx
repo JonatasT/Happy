1493c43a5523c7a3a44bdd3fc492525c0aa4d657
Jonatas Teixeira <jonatasteixeira@localhost.localdomain>
2020-10-15 19:22:21 -0300

    fix: running lint

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       as="main"
       height="100vh"
       templateColumns="1fr 5fr 8fr 2fr 1fr"
-      templateRows= "1fr 1fr 7fr 1fr 2fr"
+      templateRows="1fr 1fr 7fr 1fr 2fr"
       templateAreas="
         '. . . . .'
         '. logo lottieAnm locality .'
@@ -19,80 +19,62 @@ function App() {
       justifyContent="center"
       alignItems="center"
       backgroundImage="url('/bg.png')"
-
       backgroundRepeat="no-repeat"
       backgroundSize="cover"
       backgroundAttachment="fixed"
     >
       <Flex gridArea="logo" flexDir="column" alignItems="flex-start">
-        <img src="/logoHappyWhite.svg" alt="Happy"/>
+        <img src="/logoHappyWhite.svg" alt="Happy" />
       </Flex>
 
       <Flex gridArea="lottieAnm" height="100%">
-        <p></p>
-      </Flex>
-
-      <Flex 
-        gridArea="headline" 
-        flexDir="column" 
-        alignItems="flex-end" 
-        height="100%"
-      >
-        <Box height="100%" marginTop={260}>
-          <Heading 
-            as="h1" 
-            size="lg" 
-            lineHeight="shorter" 
-            maxWidth={600}
-          > 
-            Leve felicidade para o mundo
-          </Heading>
-          
-          <Heading 
-            as="h3" 
-            fontWeight={600} 
-            lineHeight="none" 
-            marginTop={4} 
-            maxWidth={600}
-          >
-            Visite hospitais e mude o dia de alguém para melhor 
-          </Heading>
-        </Box>
-      
-      </Flex>
-
-      <Flex 
-        gridArea="locality" 
-        flexDir="column" 
-        textAlign="right" 
-        height="100%"
-        position="relative"
-      >
-        <Heading 
-          as="p" 
-          marginTop={10} 
-          fontSize={24}
-        >
-          Niterói
-        </Heading>
-        <Heading 
-          as="p" 
-          fontWeight={400} 
-          marginTop={2} 
-          fontSize={24}
-        >
-          Rio de Janeiro
-        </Heading>
-
+        <p />
       </Flex>
 
       <Flex
-        gridArea="action" 
-        flexDir="column" 
+        gridArea="headline"
+        flexDir="column"
+        alignItems="flex-end"
+        height="100%"
+      >
+        <Box height="100%" marginTop={260}>
+          <Heading as="h1" size="lg" lineHeight="shorter" maxWidth={600}>
+            Leve felicidade para o mundo
+          </Heading>
+
+          <Heading
+            as="h3"
+            fontWeight={600}
+            lineHeight="none"
+            marginTop={4}
+            maxWidth={600}
+          >
+            Visite hospitais e mude o dia de alguém para melhor
+          </Heading>
+        </Box>
+      </Flex>
+
+      <Flex
+        gridArea="locality"
+        flexDir="column"
+        textAlign="right"
+        height="100%"
+        position="relative"
+      >
+        <Heading as="p" marginTop={10} fontSize={24}>
+          Niterói
+        </Heading>
+        <Heading as="p" fontWeight={400} marginTop={2} fontSize={24}>
+          Rio de Janeiro
+        </Heading>
+      </Flex>
+
+      <Flex
+        gridArea="action"
+        flexDir="column"
         height="100%"
         position="relative"
         textAlign="right"
-
       >
         <IconButton
           variantColor="orange"
@@ -106,8 +88,12 @@ function App() {
           borderRadius={30}
           color="white"
           transition="1s"
-          backgroundImage= "linear-gradient(329.54deg, #FF6400 0%, #FF9900 100%)"
-          _hover={{ backgroundImage: "linear-gradient(329.54deg, #FF6400 0%, #FF9900 70%)", transition: '1s' }}
+          backgroundImage="linear-gradient(329.54deg, #FF6400 0%, #FF9900 100%)"
+          _hover={{
+            backgroundImage:
+              "linear-gradient(329.54deg, #FF6400 0%, #FF9900 70%)",
+            transition: "1s",
+          }}
         />
       </Flex>
     </Grid>
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -3,9 +3,9 @@ import { theme, DefaultTheme } from "@chakra-ui/core";
 const customTheme: DefaultTheme = {
   ...theme,
   fonts: {
-    body: 'Nunito, system-ui, sans-serif',
-    heading: 'Nunito, system-ui, sans-serif',
-    mono: 'Menlo, monospace',
+    body: "Nunito, system-ui, sans-serif",
+    heading: "Nunito, system-ui, sans-serif",
+    mono: "Menlo, monospace",
   },
   fontWeights: {
     ...theme.fontWeights,
@@ -15,28 +15,28 @@ const customTheme: DefaultTheme = {
   },
   radii: {
     ...theme.radii,
-    sm: '5px',
-    md: '8px',
-    lg: '40',
+    sm: "5px",
+    md: "8px",
+    lg: "40",
   },
   fontSizes: {
     ...theme.fontSizes,
-    '2xl': '7rem',
+    "2xl": "7rem",
   },
   lineHeights: {
     ...theme.lineHeights,
-    'shorter': '0.8',
+    shorter: "0.8",
   },
   colors: {
     ...theme.colors,
     orange: {
       ...theme.colors.orange,
-      400: '#FF6400'
-    }
+      400: "#FF6400",
+    },
   },
   icons: {
     ...theme.icons,
-  }
+  },
 };
 
 export default customTheme;
