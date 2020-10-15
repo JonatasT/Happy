b46ad6579c7e070e1961b47e692eb39949039c66
Jonatas Teixeira <jonatasteixeira@localhost.localdomain>
2020-10-15 11:43:41 -0300

    feat: adding theme style in themecontainer

--- contexts/theme/ThemeContainer.tsx
+++ contexts/theme/ThemeContainer.tsx
@@ -3,11 +3,13 @@ import React from 'react';
 import { ColorModeProvider, CSSReset, ThemeProvider as ChakraThemeProvider } from '@chakra-ui/core';
 import { ThemeProvider as EmotionThemeProvider } from 'emotion-theming';
 
+import theme from '../../styles/theme';
+
 const ThemeContainer: React.FC = ({children}) => {
     return (
-        <ChakraThemeProvider theme={}>
+        <ChakraThemeProvider theme={theme}>
             <ColorModeProvider value="light">
-                <EmotionThemeProvider theme={{}}>
+                <EmotionThemeProvider theme={theme}>
                     <CSSReset/>
                     {children}
                 </EmotionThemeProvider>
