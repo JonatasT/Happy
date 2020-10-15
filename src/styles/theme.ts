d7e417a51aa8d27c6ab4f80aa44f374809a098cf
Jonatas Teixeira <jonatasteixeira@localhost.localdomain>
2020-10-15 13:34:57 -0300

    feat: adding initial theme variables

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,6 +2,34 @@ import { theme, DefaultTheme } from "@chakra-ui/core";
 
 const customTheme: DefaultTheme = {
   ...theme,
+  fonts: {
+    body: 'Nunito, system-ui, sans-serif',
+    heading: 'Nunito, system-ui, sans-serif',
+    mono: 'Menlo, monospace',
+  },
+  fontWeights: {
+    ...theme.fontWeights,
+    normal: 400,
+    medium: 700,
+    bold: 800,
+  },
+  radii: {
+    ...theme.radii,
+    sm: '5px',
+    md: '8px',
+    lg: '40px',
+  },
+  fontSizes: {
+    ...theme.fontSizes,
+    '6xl': '54px',
+  },
+  colors: {
+    ...theme.colors,
+    orange: {
+      ...theme.colors.orange,
+      400: '#FF6400'
+    }
+  }
 };
 
 export default customTheme;
