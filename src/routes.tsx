d670d0195dcf49db7a582bc7b661fc3aa6c36464
Jonatas Teixeira <jonatasteixeira@localhost.localdomain>
2020-10-16 11:44:25 -0300

    chore: adding hospitalsmap to the routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import Landing from "./pages/Landing";
+import HospitalsMap from "./pages/HospitalsMap";
 
 function Routes() {
     return(
         <BrowserRouter>
             <Switch>
                 <Route path="/" exact component={Landing}/>
+                <Route path="/app" component={HospitalsMap}/>
             </Switch>
         </BrowserRouter>
     );
